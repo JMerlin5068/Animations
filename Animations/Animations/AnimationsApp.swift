429339e18655914dd87ce5c0d7b0402e0e741f74
JMerlin5068 <155556728+JMerlin5068@users.noreply.github.com>
2024-02-29 08:07:03 -0600

    Add files via upload

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Student on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
